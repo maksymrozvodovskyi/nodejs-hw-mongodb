d99b3309fb3c79e10dd643780427951882c3a084
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-07-08 15:13:18 +0300

    end 3 and 4 step

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import { startServer } from './server.js';
+import { initMongoConnection } from './db/initMongoConnection.js';
 
-startServer();
+const run = async () => {
+  await initMongoConnection();
+  startServer();
+};
+
+run();
