151ec6e453d25c341d4be36ea4cc600f7d783a6c
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-07-09 01:17:28 +0300

    fix response

--- src/server.js
+++ src/server.js
@@ -23,6 +23,7 @@ export const startServer = () => {
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
     res.status(200).json({
+      status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
     });
@@ -38,6 +39,7 @@ export const startServer = () => {
     }
 
     res.status(200).json({
+      status: 200,
       message: `Successfully found contact with id ${contactId}!`,
       data: contact,
     });
