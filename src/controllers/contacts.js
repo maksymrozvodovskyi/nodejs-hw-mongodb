a3913d53b10af0857b3dfc21251056f6c24ca7ec
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-08-07 19:01:54 +0300

    save

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -50,7 +50,22 @@ export const getContactByIdController = async (req, res) => {
 };
 
 export const createContactController = async (req, res) => {
-  const contact = await createContact({ ...req.body, userId: req.user.id });
+  const photo = req.file;
+  let photoUrl;
+
+  if (photo) {
+    if (getEnvVar('ENABLE_CLOUDINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    } else {
+      photoUrl = await saveFileToUploadDir(photo);
+    }
+  }
+
+  const contact = await createContact({
+    ...req.body,
+    photo: photoUrl,
+    userId: req.user.id,
+  });
 
   res.status(201).json({
     status: 201,
@@ -114,8 +129,6 @@ export const patchContactController = async (req, res, next) => {
     ...req.body,
     photo: photoUrl,
   });
-
-  // const result = await updateContact(contactId, req.body, {}, req.user.id);
 
   if (!result) {
     next(createHttpError(404, 'Contact not found'));
