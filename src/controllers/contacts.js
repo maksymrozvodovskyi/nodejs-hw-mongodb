3142563e250ac2f7a2edf750429e82fa9457bdca
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-08-08 14:45:56 +0300

    save

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -125,10 +125,15 @@ export const patchContactController = async (req, res, next) => {
     }
   }
 
-  const result = await updateContact(contactId, {
-    ...req.body,
-    photo: photoUrl,
-  });
+  const result = await updateContact(
+    contactId,
+    {
+      ...req.body,
+      photo: photoUrl,
+    },
+    {},
+    req.user.id,
+  );
 
   if (!result) {
     next(createHttpError(404, 'Contact not found'));
